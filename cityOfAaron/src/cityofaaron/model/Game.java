02698e37e5859e4ac9cd9bb68e3011449eeb5108
kent <kent@DESKTOP-LJ4T71O>
2018-05-15 20:31:07 -0700

    15 May 2018 - first model classes - group work

---
+++ cityOfAaron/src/cityofaaron/model/Game.java
@@ -0,0 +1,127 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cityofaaron.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+/**
+ *
+ * @author kent
+ */
+public class Game implements Serializable {
+    // class instance variables
+    private Player thePlayer;
+    private Map theMap;
+    private Storehouse theStorehouse;
+    private int currentPopulation;
+    private int wheatInStore;
+    private int currentYear;
+    
+    //javabean code
+    public Game() {
+    }
+
+    public Player getThePlayer() {
+        return thePlayer;
+    }
+
+    public void setThePlayer(Player thePlayer) {
+        this.thePlayer = thePlayer;
+    }
+
+    public Map getTheMap() {
+        return theMap;
+    }
+
+    public void setTheMap(Map theMap) {
+        this.theMap = theMap;
+    }
+
+    public Storehouse getTheStorehouse() {
+        return theStorehouse;
+    }
+
+    public void setTheStorehouse(Storehouse theStorehouse) {
+        this.theStorehouse = theStorehouse;
+    }
+
+    public int getCurrentPopulation() {
+        return currentPopulation;
+    }
+
+    public void setCurrentPopulation(int currentPopulation) {
+        this.currentPopulation = currentPopulation;
+    }
+
+    public int getWheatInStore() {
+        return wheatInStore;
+    }
+
+    public void setWheatInStore(int wheatInStore) {
+        this.wheatInStore = wheatInStore;
+    }
+
+    public int getCurrentYear() {
+        return currentYear;
+    }
+
+    public void setCurrentYear(int currentYear) {
+        this.currentYear = currentYear;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 59 * hash + Objects.hashCode(this.thePlayer);
+        hash = 59 * hash + Objects.hashCode(this.theMap);
+        hash = 59 * hash + Objects.hashCode(this.theStorehouse);
+        hash = 59 * hash + this.currentPopulation;
+        hash = 59 * hash + this.wheatInStore;
+        hash = 59 * hash + this.currentYear;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Game other = (Game) obj;
+        if (this.currentPopulation != other.currentPopulation) {
+            return false;
+        }
+        if (this.wheatInStore != other.wheatInStore) {
+            return false;
+        }
+        if (this.currentYear != other.currentYear) {
+            return false;
+        }
+        if (!Objects.equals(this.thePlayer, other.thePlayer)) {
+            return false;
+        }
+        if (!Objects.equals(this.theMap, other.theMap)) {
+            return false;
+        }
+        if (!Objects.equals(this.theStorehouse, other.theStorehouse)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" + "thePlayer=" + thePlayer + ", theMap=" + theMap + ", theStorehouse=" + theStorehouse + ", currentPopulation=" + currentPopulation + ", wheatInStore=" + wheatInStore + ", currentYear=" + currentYear + '}';
+    }
+
+    
+}
---
+++ cityOfAaron/src/cityofaaron/model/Player.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cityofaaron.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Objects;
+
+/**
+ *
+ * @author kent
+ */
+public class Player implements Serializable{
+    //class instance variables
+    private String name;
+    private ArrayList<Game> games = new ArrayList<Game>();
+    
+    
+    //javabean code
+    public Player() {
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<Game> getGames() {
+        return games;
+    }
+
+    public void setGames(ArrayList<Game> games) {
+        this.games = games;
+    }
+    
+    
+
+    @Override
+    public String toString() {
+        return "Player{" + "name=" + name + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 83 * hash + Objects.hashCode(this.name);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Player other = (Player) obj;
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return true;
+    }
+    //end javabean code
+    
+    
+    
+    
+}
