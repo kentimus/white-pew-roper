ac7324c8b6d04bdc078ad222096e229bfd8f54d7
Hayden <>
2018-05-15 21:55:26 -0700

    Hayden 5/14/2018

---
+++ cityOfAaron/src/cityofaaron/model/Storehouse.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cityofaaron.model;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author pewst
+ */
+public class Storehouse implements Serializable {
+    
+    //Class instance variables
+    private Author authors;
+    private InventoryItem animals ;
+    private InventoryItem tools ;
+    private InventoryItem provisions ;
+    
+    // Javabean code
+    
+   
+    
+}
