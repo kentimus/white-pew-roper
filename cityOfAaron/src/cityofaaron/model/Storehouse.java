02f28d361b84edd55f8310eda56c36a5549b5906
Hayden <pewst@192.168.3.107>
2018-05-16 21:17:29 -0700

    Hayden 5/16/2018

--- cityOfAaron/src/cityofaaron/model/Storehouse.java
+++ cityOfAaron/src/cityofaaron/model/Storehouse.java
@@ -6,6 +6,7 @@
 package cityofaaron.model;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 /**
  *
@@ -26,11 +27,85 @@ public class Storehouse implements Serializable {
     }
     
     //Getters and setters
+
+    public Author getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Author authors) {
+        this.authors = authors;
+    }
+
+    public InventoryItem getAnimals() {
+        return animals;
+    }
+
+    public void setAnimals(InventoryItem animals) {
+        this.animals = animals;
+    }
+
+    public InventoryItem getTools() {
+        return tools;
+    }
+
+    public void setTools(InventoryItem tools) {
+        this.tools = tools;
+    }
+
+    public InventoryItem getProvisions() {
+        return provisions;
+    }
+
+    public void setProvisions(InventoryItem provisions) {
+        this.provisions = provisions;
+    }
+    
+    // hashcode and equals
+        @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 83 * hash + Objects.hashCode(this.authors);
+        hash = 83 * hash + Objects.hashCode(this.animals);
+        hash = 83 * hash + Objects.hashCode(this.tools);
+        hash = 83 * hash + Objects.hashCode(this.provisions);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Storehouse other = (Storehouse) obj;
+        if (!Objects.equals(this.authors, other.authors)) {
+            return false;
+        }
+        if (!Objects.equals(this.animals, other.animals)) {
+            return false;
+        }
+        if (!Objects.equals(this.tools, other.tools)) {
+            return false;
+        }
+        if (!Objects.equals(this.provisions, other.provisions)) {
+            return false;
+        }
+        return true;
+    }
+    
+    // to string
+
+    @Override
+    public String toString() {
+        return "Storehouse{" + "authors=" + authors + ", animals=" + animals + ", tools=" + tools + ", provisions=" + provisions + '}';
+    }
+    
     
 
     
     }
-    
-   
-    
-}
