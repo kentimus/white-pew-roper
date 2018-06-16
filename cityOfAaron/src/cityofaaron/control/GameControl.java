f3de79979128d5b3ca4ce28fe1b65806693c6342
pewst <pewst@192.168.3.107>
2018-06-15 19:01:10 -0700

    Hayden 6/15

--- cityOfAaron/src/cityofaaron/control/GameControl.java
+++ cityOfAaron/src/cityofaaron/control/GameControl.java
@@ -4,9 +4,9 @@
  * and open the template in the editor.
  */
 package cityofaaron.control;
+
 import cityofaaron.model.Game;
 import java.util.Random;
-
 
 /**
  *
@@ -14,27 +14,25 @@ import java.util.Random;
  */
 public class GameControl {
 
-    public int newPopulation (int currentPopulation){
+    public int newPopulation(int currentPopulation) {
         //Kent
         int addedPopulation;
         int newPopulation;
-        
+
         //currentPopulation needs to be greater than zero
-        if(currentPopulation <= 0){
+        if (currentPopulation <= 0) {
             return -1;
         }
-    
+
         Random rand = new Random(0);
         // get randomNumber between 1 and 5, inclusive
         int randomNum = rand.nextInt(5) + 1;
         addedPopulation = (currentPopulation * randomNum) / 100;
         newPopulation = addedPopulation + currentPopulation;
-            return newPopulation;
-}
+        return newPopulation;
+    }
 
-   
-    
-    public int pricePerAcre(){
+    public int pricePerAcre() {
         //kent
         Random rand = new Random();
         // get randomNumber between 17 and 27, inclusive
@@ -42,148 +40,140 @@ public class GameControl {
         return pricePerAcre;
     }
 
-
-
-    public int sellAcres(int acresToSell, int pricePerAcre, int acresOwned){
-    //Hayden   
+    public int sellAcres(int acresToSell, int pricePerAcre, int acresOwned) {
+        //Hayden   
         if (acresToSell < 0 || acresToSell > acresOwned) {
             return -1;
-        }
-        
-        else { 
+        } else {
             int wheatFromAcres = (acresToSell * pricePerAcre);
-		return wheatFromAcres;
+            return wheatFromAcres;
         }
     }
-    
-    public int buyAcres(int acresRequested, int pricePerAcre, int wheatInStore){
-    //group
-                
-	if (acresRequested < 0) {
+
+    public int buyAcres(int acresRequested, int pricePerAcre, int wheatInStore) {
+        //group
+
+        if (acresRequested < 0) {
             return -1;
         }
 
-	// get request cost
-	int cost = (acresRequested * pricePerAcre);
-	if (cost > wheatInStore) {
+        // get request cost
+        int cost = (acresRequested * pricePerAcre);
+        if (cost > wheatInStore) {
             //not enough wheat in store
             return -1;
-        }
-        else{ int acresBought = acresRequested;
+        } else {
+            int acresBought = acresRequested;
             return acresBought;
         }
     }
 
-public int acresPlanted(int acresRequested, int wheatInStore){
-    //will
-    Random rand = new Random();
-    int acresPlanted = 0;
-    int low;
-    int high;
+    public int acresPlanted(int acresRequested, int wheatInStore) {
+        //will
+        Random rand = new Random();
+        int acresPlanted;
+        int low;
+        int high;
 
-	if (acresRequested < 0) {
+        if (acresRequested < 0) {
             return -1;
         }
-            low = 17;
-            high = 27;
-        int cost = (acresRequested * (rand.nextInt(high - low ) + low));
-	if (cost > wheatInStore) {
+        low = 17;
+        high = 27;
+        int cost = (acresRequested * (rand.nextInt(high - low) + low));
+        if (cost > wheatInStore) {
             //not enough wheat in store
             return -1;
-        }
-        else{acresPlanted = acresRequested;
+        } else {
+            acresPlanted = acresRequested;
             return acresPlanted;
         }
     }
 
-public int wheatHarvested(int acresPlanted, double tithingPaid){
+    public int wheatHarvested(int acresPlanted, double tithingPaid) {
 //will
-    int wheatHarvested = 0; 
-    Random rand = new Random();
-    int low;
-    int high;
-    
-    
-        if(tithingPaid < .08){
+        int wheatHarvested = 0;
+        Random rand = new Random();
+        int low;
+        int high;
+
+        if (tithingPaid < .08) {
             low = 1;
             high = 3;
             // 1-3 bushels harvested
-            wheatHarvested =  (rand.nextInt(high - low ) + low)* acresPlanted;
+            wheatHarvested = (rand.nextInt(high - low) + low) * acresPlanted;
         }
-        if(tithingPaid > .12){
+        if (tithingPaid > .12) {
             low = 2;
             high = 5;
             // 2-5 bushels harvested
-            wheatHarvested =  (rand.nextInt(high - low ) + low)* acresPlanted;
+            wheatHarvested = (rand.nextInt(high - low) + low) * acresPlanted;
         }
-        if(tithingPaid >= .08 && tithingPaid <= .12){
+        if (tithingPaid >= .08 && tithingPaid <= .12) {
             low = 2;
             high = 4;
             // 2-4 bushels harvested
-            wheatHarvested =  (rand.nextInt(high - low ) + low)* acresPlanted;
-        }	
-            return wheatHarvested;
+            wheatHarvested = (rand.nextInt(high - low) + low) * acresPlanted;
+        }
+        return wheatHarvested;
     }
 
     // argument "rando" temporary for testing
-    public double wheatEatenByRats(int wheatInStore, double tithingPaid, int rando){
+    public double wheatEatenByRats(int wheatInStore, double tithingPaid, int rando) {
         //Kent
         int percentEaten = 0;
         int bushelsEaten;
-        
-            // wheatInStore can't be below zero
-            if(wheatInStore  < 0){
-                return -1;
-            }
-        
-            // tithingPaid can't be below zero
-            if(tithingPaid < 0){
-                return -1;
-            }
+
+        // wheatInStore can't be below zero
+        if (wheatInStore < 0) {
+            return -1;
+        }
+
+        // tithingPaid can't be below zero
+        if (tithingPaid < 0) {
+            return -1;
+        }
         // create instance of Random class
         Random rand = new Random(5);
         //int randomNum = rand.nextInt(100);
-        
+
         //temporary line, for testing
         int randomNum = rando;
-            if(randomNum  < 30){
-                if(tithingPaid < .08){
+        if (randomNum < 30) {
+            if (tithingPaid < .08) {
                 // Generate random percent between 6% and 10%
-                percentEaten =  rand.nextInt(5) + 6;
-                }
-                if(tithingPaid > .12){
+                percentEaten = rand.nextInt(5) + 6;
+            }
+            if (tithingPaid > .12) {
                 // Generate randome percent between 3% and 5%
                 percentEaten = rand.nextInt(3) + 3;
-                }
-                if(tithingPaid >= .08 && tithingPaid <= .12){
+            }
+            if (tithingPaid >= .08 && tithingPaid <= .12) {
                 // tithingPaid between 8% and 12%, inclusive
                 // Generate random percent between 3% and 7%
                 percentEaten = rand.nextInt(5) + 3;
-                }		
-            } 
-		
-            else{ // randomNum not less than 30
+            }
+        } else { // randomNum not less than 30
             percentEaten = 0;
         }
-	bushelsEaten = (percentEaten * wheatInStore) /100;
-	return bushelsEaten;
+        bushelsEaten = (percentEaten * wheatInStore) / 100;
+        return bushelsEaten;
     }
 
-    public int starvedPopulation(int currentPopulation, int feedPopulation, int wheatInStore){
+    public int starvedPopulation(int currentPopulation, int feedPopulation, int wheatInStore) {
         //Hayden
         int totalWheatNeeded = currentPopulation * 20;
         int starvedPopulation;
 
         if (feedPopulation < 0 || feedPopulation > wheatInStore) {
             return -1;
-           }
-        if (totalWheatNeeded > wheatInStore){
+        }
+        if (totalWheatNeeded > wheatInStore) {
             starvedPopulation = currentPopulation - (wheatInStore / 20);
-            return starvedPopulation; 
-        }
-        else starvedPopulation =  currentPopulation - (feedPopulation / 20);
             return starvedPopulation;
+        } else {
+            starvedPopulation = currentPopulation - (feedPopulation / 20);
         }
+        return starvedPopulation;
     }
-    
-
+}
