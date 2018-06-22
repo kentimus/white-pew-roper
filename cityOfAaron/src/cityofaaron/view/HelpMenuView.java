d5d6e5552ad872279b61836848a69436a1f7377d
kent <kent@DESKTOP-LJ4T71O>
2018-06-22 12:16:34 -0700

    Kent - refactoring HelpMenuView

--- cityOfAaron/src/cityofaaron/view/HelpMenuView.java
+++ cityOfAaron/src/cityofaaron/view/HelpMenuView.java
@@ -1,26 +1,16 @@
-
 package cityofaaron.view;
 
-import java.util.Scanner;
-
-/**
- *
- * @author kanderson
- */
-public class HelpMenuView {
-    
-    
-    /**
-     * The message that will be displayed by this view.
-     */
-    protected String message;
-    
+public class HelpMenuView extends ViewBase {
     /**
      * Constructor
      */
     public HelpMenuView(){
-        
-        message = "This is the Help Menu. It is here to help you.\n"
+        super();
+    }
+    
+    @Override
+    public String getMessage(){
+        return "This is the Help Menu. It is here to help you.\n"
                 + "------------------------------------------------------------------------\n"
                 + "1 - What are the goals of the game?\n"
                 + "2 - Where is the city of Aaron?\n"
@@ -31,60 +21,14 @@ public class HelpMenuView {
                 + "\n"
                 + "6 - Never mind; this menu isn't helpful. Take me back to the Main Menu.\n"
                 + "-------------------------------------------------------------------------";
-                
     }
-    
-    
-    /**
-     * Get the user's input. Keep prompting them until they enter a value.
-     * @param prompt
-     * @param allowEmpty - determine whether the user can enter no value (just a return key)
-     * @return 
-     */
-    protected String getUserInput(String prompt, boolean allowEmpty){
-        
-        Scanner keyboard = new Scanner(System.in);
-        String input = "";
-        boolean inputReceived = false;
-        
-        while(inputReceived == false){
-            
-            System.out.println(prompt);
-            input = keyboard.nextLine();
-            
-            // Make sure we avoid a null-pointer error.
-            if (input == null){
-                input = "";
-            }
-            
-            // Trim any trailing whitespace, including the carriage return.
-            input = input.trim();
-            
-            if (input.equals("") == false || allowEmpty == true){
-                inputReceived = true;
-            }
-        }
-        
-        return input;
-    }
-    
-    
-    /**
-     * An overloaded version of getUserInput that sets allowEmpty to false so we don't have 
-     * to type it ourselves.
-     * @param prompt
-     * @return 
-     */
-    protected String getUserInput(String prompt){
-        return getUserInput(prompt, false);
-    }
-    
+     
     /**
      * Get the set of inputs from the user.
      * @return 
      */
+    @Override
     public String[] getInputs() {
-        
         // Declare the array to have the number of elements you intend to get 
         // from the user.
         String[] inputs = new String[1];
@@ -103,6 +47,7 @@ public class HelpMenuView {
      * @return true if the view should repeat itself, and false if the view
      * should exit and return to the previous view.
      */
+    @Override
     public boolean doAction(String[] inputs){
         switch (inputs[0].trim().toUpperCase() ){
             case "1":
@@ -126,36 +71,10 @@ public class HelpMenuView {
                 System.out.println("\"" + inputs[0].trim() + "\" wasn't a valid choice.");
                 System.out.println("That wasn't a number between 1 and 6. Try again.\n");
         }
-        
-        
+            
         return true;
     }
-    
-    
-    /**
-     * Control this view's display/prompt/action loop until the user
-     * chooses and action that causes this view to close.
-     */
-    public void displayView(){
         
-        boolean keepGoing = true;
-        
-        while(keepGoing == true){
-            
-            System.out.println(message);
-            String[] inputs = getInputs();
-            keepGoing = doAction(inputs);
-        }
-    }
-    
-    
-    // Define your action handlers here. These are the methods that your doAction()
-    // method will call based on the user's input. We don't want to do a lot of 
-    // complex game stuff in our doAction() method. It will get messy very quickly.
-    
-    
-    
-    
     private void helpGameGoals(){
         System.out.println ("\nGame Goals:\n"
                 + "1: Don't let your people starve\n"
