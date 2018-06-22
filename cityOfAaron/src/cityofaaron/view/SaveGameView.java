b18ed71ee5eb828e15ceae93dbfb95e82d91ffcb
kent <kent@DESKTOP-LJ4T71O>
2018-06-22 12:26:37 -0700

    Kent - refactoring StartGameView

--- cityOfAaron/src/cityofaaron/view/SaveGameView.java
+++ cityOfAaron/src/cityofaaron/view/SaveGameView.java
@@ -1,86 +1,34 @@
-
 package cityofaaron.view;
-
-import java.util.Scanner;
 
 /**
  *
- * @author kanderson
+ * @author kent roper
  */
-public class SaveGameView {
-    
-    
-    /**
-     * The message that will be displayed by this view.
-     */
-    protected String message;
-    
+public class SaveGameView extends ViewBase {
     /**
      * Constructor
      */
     public SaveGameView(){
-        
-        message = "Please enter a file name to save your game ...\n";
+        super();
+    }
+    
+    @Override
+    public String getMessage(){
+        return "Please enter a file name to save your game ...\n";
                                
-    }
-    
-    
-    /**
-     * Get the user's input. Keep prompting them until they enter a value.
-     * @param prompt
-     * @param allowEmpty - determine whether the user can enter no value (just a return key)
-     * @return 
-     */
-    protected String getUserInput(String prompt, boolean allowEmpty){
-        
-        Scanner keyboard = new Scanner(System.in);
-        String input = "";
-        boolean inputReceived = false;
-        
-        while(inputReceived == false){
-            
-            System.out.println(prompt);
-            input = keyboard.nextLine();
-            
-            // Make sure we avoid a null-pointer error.
-            if (input == null){
-                input = "";
-            }
-            
-            // Trim any trailing whitespace, including the carriage return.
-            input = input.trim();
-            
-            if (input.equals("") == false || allowEmpty == true){
-                inputReceived = true;
-            }
-        }
-        
-        return input;
-    }
-    
-    
-    /**
-     * An overloaded version of getUserInput that sets allowEmpty to false so we don't have 
-     * to type it ourselves.
-     * @param prompt
-     * @return 
-     */
-    protected String getUserInput(String prompt){
-        return getUserInput(prompt, false);
     }
     
     /**
      * Get the set of inputs from the user.
      * @return 
      */
+    @Override
     public String[] getInputs() {
-        
         // Declare the array to have the number of elements you intend to get 
         // from the user.
         String[] inputs = new String[1];
         
         inputs[0] = getUserInput("", true);
-        
         
         // Repeat for each input you need, putting it into its proper slot in the array.
         
@@ -94,6 +42,7 @@ public class SaveGameView {
      * @return true if the view should repeat itself, and false if the view
      * should exit and return to the previous view.
      */
+    @Override
     public boolean doAction(String[] inputs){
         // there is only one action here, start a new game and set it in the main cityofaaron class
         
@@ -107,31 +56,8 @@ public class SaveGameView {
         String fileName = inputs[0];
         saveGame(fileName);
         
-        
         return false;
     }
-    
-    
-    /**
-     * Control this view's display/prompt/action loop until the user
-     * chooses and action that causes this view to close.
-     */
-    public void displayView(){
-        
-        boolean keepGoing = true;
-        
-        while(keepGoing == true){
-            
-            System.out.println(message);
-            String[] inputs = getInputs();
-            keepGoing = doAction(inputs);
-        }
-    }
-    
-    
-    // Define your action handlers here. These are the methods that your doAction()
-    // method will call based on the user's input. We don't want to do a lot of 
-    // complex game stuff in our doAction() method. It will get messy very quickly.
     
     
     private void saveGame(String fileName){
