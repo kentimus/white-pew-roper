728e09d4fbaa33468f8064343ca89153b3036b0d
pewst <pewst@192.168.3.107>
2018-06-21 21:37:57 -0700

    Hayden 6/21

--- cityOfAaron/src/cityofaaron/view/ReportsMenuView.java
+++ cityOfAaron/src/cityofaaron/view/ReportsMenuView.java
@@ -1,61 +1,62 @@
-
 package cityofaaron.view;
-
-import java.util.Scanner;
 
 /**
  *
  * @author kanderson
  * @author hpew
  */
-public class ReportsMenuView extends ViewBase{
-    
+public class ReportsMenuView extends ViewBase {
+
     /**
      * Constructor
      */
-    public ReportsMenuView(){
+    public ReportsMenuView() {
         super();
     }
+
+    @Override
+    protected String getMessage() {
+        return "Reports Menu\n"
+                + "-----------------------------------------\n"
+                + "1 - View animals in the Storehouse\n"
+                + "2 - View the tools in the storehouse\n"
+                + "3 - View the provisions in the storehouse\n"
+                + "4 - View the authors of this game\n"
+                + "\n"
+                + "5 - Return to the Game Menu.\n"
+                + "-----------------------------------------\n";
+    }
+
+    /**
+     * Get the set of inputs from the user.
+     *
+     * @return
+     */
     
     @Override
-    protected String getMessage(){ 
-        return "Reports Menu\n"
-               + "-----------------------------------------\n"
-               + "1 - View animals in the Storehouse\n"
-               + "2 - View the tools in the storehouse\n"
-               + "3 - View the provisions in the storehouse\n"
-               + "4 - View the authors of this game\n"
-               + "\n"
-               + "5 - Return to the Game Menu.\n"
-               + "-----------------------------------------\n";
-    }
-        
-     /**
-     * Get the set of inputs from the user.
-     * @return 
-     */
     public String[] getInputs() {
-        
+
         // Declare the array to have the number of elements you intend to get 
         // from the user.
         String[] inputs = new String[1];
-        
+
         inputs[0] = getUserInput("Please make a selection");
-        
+
         // Repeat for each input you need, putting it into its proper slot in the array.
-        
         return inputs;
     }
-    
-    
+
     /**
      * Perform the action indicated by the user's input.
+     *
      * @param inputs
      * @return true if the view should repeat itself, and false if the view
      * should exit and return to the previous view.
      */
-    public boolean doAction(String[] inputs){
-        switch (inputs[0].trim().toUpperCase() ){
+    
+    @Override
+    public boolean doAction(String[] inputs) {
+        switch (inputs[0].trim().toUpperCase()) {
             case "1":
                 animalsInStorehouse();
                 break;
@@ -70,43 +71,41 @@ public class ReportsMenuView extends ViewBase{
                 break;
             case "5": // return to main menu
                 return false;
-            default : 
+            default:
                 System.out.println("\"" + inputs[0].trim() + "\" wasn't a valid choice.");
                 System.out.println("That wasn't a number between 1 and 5. Try again.\n");
         }
         return true;
     }
-    
+
     // Define your action handlers here. These are the methods that your doAction()
     // method will call based on the user's input. We don't want to do a lot of 
     // complex game stuff in our doAction() method. It will get messy very quickly.
-    
-    private void animalsInStorehouse(){
-        System.out.println ("\n**Animals in storehouse**\n"
+    private void animalsInStorehouse() {
+        System.out.println("\n**Animals in storehouse**\n"
                 + "This will eventually show the animals in the storehouse\n"
                 + "And the user will be able to save the report\n"
                 + "-------------------------------------------------------\n");
     }
-    
-    private void toolsInStorehouse(){
-        System.out.println ("\n**Tools in storehouse**\n"
+
+    private void toolsInStorehouse() {
+        System.out.println("\n**Tools in storehouse**\n"
                 + "This will eventually show the tools in the storehouse\n"
                 + "And the user will be able to save the report\n"
                 + "-----------------------------------------------------\n");
     }
-    
-    private void provisionsInStorehouse(){
+
+    private void provisionsInStorehouse() {
         System.out.println("\n**Provisions in storehouse**\n"
                 + "This will eventually show the provisions in the storehouse\n"
                 + "And the user will be able to save the report\n"
                 + "----------------------------------------------------------\n");
     }
-    
-    private void viewAuthors(){
+
+    private void viewAuthors() {
         System.out.println("\n**Game Authors**\n"
                 + "\n"
                 + "This will eventually show the authors of the game\n"
                 + "-------------------------------------------------\n");
     }
 }
-
--- cityOfAaron/src/cityofaaron/view/ViewMapView.java
+++ cityOfAaron/src/cityofaaron/view/ViewMapView.java
@@ -1,70 +1,72 @@
 
 package cityofaaron.view;
 
-import java.util.Scanner;
-
 /**
  *
  * @author kanderson
  * @author hpew
  */
-public class ViewMapView extends ViewBase{
-    
+public class ViewMapView extends ViewBase {
+
     /**
      * Constructor
      */
-    public ViewMapView(){
+    public ViewMapView() {
         super();
     }
-        
-    protected String getMessage(){
+
+    @Override
+    protected String getMessage() {
         return "** The City Of Aaron **\n"
-               + "-W- -R- --- --- -U- -L-\n"
-               + "--- -W- -R- --- -U- -L-\n"
-               + "--- -W- -W- -R- -U- -L-\n"
-               + "--- -V- -W- W-R -U- -L-\n"
-               + "--- -G- -W- -R- -U- -L-\n"
-               + "--- --- --- --R --- -L-\n"
-               + "--- --- --- R-- --- ---\n"
-               + "W = wheat field      R = river\n"
-               + "L = Lamanite border  V = village\n"
-               + "U = undeveloped land G = Granary\n";       
+                + "-W- -R- --- --- -U- -L-\n"
+                + "--- -W- -R- --- -U- -L-\n"
+                + "--- -W- -W- -R- -U- -L-\n"
+                + "--- -V- -W- W-R -U- -L-\n"
+                + "--- -G- -W- -R- -U- -L-\n"
+                + "--- --- --- --R --- -L-\n"
+                + "--- --- --- R-- --- ---\n"
+                + "W = wheat field      R = river\n"
+                + "L = Lamanite border  V = village\n"
+                + "U = undeveloped land G = Granary\n";
     }
-    
+
     /**
      * Get the set of inputs from the user.
-     * @return 
+     *
+     * @return
      */
+    
+    @Override
     public String[] getInputs() {
-        
+
         //no inputs from this view, just diplay the map and return to the GameView
         return null;
     }
-    
-    
+
     /**
      * Perform the action indicated by the user's input.
+     *
      * @param inputs
      * @return true if the view should repeat itself, and false if the view
      * should exit and return to the previous view.
      */
-    public boolean doAction(String[] inputs){
+    
+    @Override
+    public boolean doAction(String[] inputs) {
         // no action jusr dispaly the map
-        
+
         startGameMenuView();
-        
+
         return false;
     }
-        
+
     // Define your action handlers here. These are the methods that your doAction()
     // method will call based on the user's input. We don't want to do a lot of 
     // complex game stuff in our doAction() method. It will get messy very quickly.
-    
-    
-    private void startGameMenuView(){
+    private void startGameMenuView() {
         //pause for a few seconds and return to Game Menu
         pause(3000);
-        
+
         GameMenuView gameMenu = new GameMenuView();
         gameMenu.displayView();
     }
