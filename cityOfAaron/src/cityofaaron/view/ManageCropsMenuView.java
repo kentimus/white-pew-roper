ad5b9aa901a96afec401b3ae1c659469e063daf7
willi <willi@LAPTOP-RAN669NP>
2018-06-23 22:53:38 -0600

    6/23

--- cityOfAaron/src/cityofaaron/view/ManageCropsMenuView.java
+++ cityOfAaron/src/cityofaaron/view/ManageCropsMenuView.java
@@ -94,31 +94,31 @@ public class ManageCropsMenuView extends ViewBase{
     */
     private void buyLand(){
        System.out.println("\n**Buy Land**\n"
-                + "This will Allow the user to enter the number of acres to buy\n"
+                + "This will allow the user to enter the number of acres to buy\n"
                 + "-------------------------------------------------------\n");
     }
     
     private void sellLand(){
         System.out.println("\n**Buy Land**\n"
-                + "This will Allow the user to enter the number of acres to sell if no areas are bought\n"
+                + "This will allow the user to enter the number of acres to sell if no areas are bought\n"
                 + "-------------------------------------------------------\n");
     }
     
     private void feedThePeople(){
         System.out.println("\n**Buy Land**\n"
-                + "This will Allow the user to enter the number of wheat to feed the population\n"
+                + "This will allow the user to enter the number of wheat to feed the population\n"
                 + "-------------------------------------------------------\n");
     }
     
     private void plantCrops(){
        System.out.println("\n**Buy Land**\n"
-                + "This will Allow the user to enter the number of acres to plant\n"
+                + "This will allow the user to enter the number of acres to plant\n"
                 + "-------------------------------------------------------\n");
     }
     
     private void payTithesAndOfferings(){
        System.out.println("\n**Buy Land**\n"
-                + "This will Allow the user to enter the amount of wheat to tithe or offer\n"
+                + "This will allow the user to enter the amount of wheat to tithe or offer\n"
                 + "-------------------------------------------------------\n");
     }
 
